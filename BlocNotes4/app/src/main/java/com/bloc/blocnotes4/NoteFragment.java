510731ff8fb8fe56dada30364bf08b953dadc98c
Giao Phung <giaophung@gmail.com>
2014-10-14 11:28:31 -0400

    Completed My First Application Checkpoint

---
+++ BlocNotes4/app/src/main/java/com/bloc/blocnotes4/NoteFragment.java
@@ -0,0 +1,12 @@
+package com.bloc.blocnotes4;
+
+import android.app.Fragment;
+
+/**
+ * Created by giao on 10/14/14.
+ */
+public class NoteFragment extends Fragment {
+    NoteFragment () {
+
+    }
+}
